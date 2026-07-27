a31e0159c97d63145963e27d696c73aea8e956cb
XnoctisScript <qptrilig@gmail.com>
2026-07-27 17:53:19 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,2 @@
-if not game:IsLoaded() then 
- game.Loaded:Wait() end 
-getgenv().SCRIPT_KEY = "KEYLESS"
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/f6847b778636bd6696e04faee2b18657a3b6b869e515e917f0632e6bd9e76a95/download"))()
+lp_key = "05182c6e3178517a7ff9000b"
+loadstring(game:HttpGet("https://luaprot.net/api/v2/loaders/get/40133528332003734197"))()
